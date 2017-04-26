b279d8861a1760fbebb3e22ce192ddc62e7c8937
KhalidOmer <khalid.hassan@aims.ac.rw>
2017-04-26 17:02:15 +0200

    import patches

--- partonshower3d.py
+++ partonshower3d.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.patches as mpatches
 from numpy.linalg import norm
 import datetime
 import time
